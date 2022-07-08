2937450dd1da46d7ba346854b0b9bb9c7acf2688
Hunter Padgett <gizmokabanjo@gmail.com>
2022-07-07 23:42:37 -0400

    added newsletter feature

--- assets/script.js
+++ assets/script.js
@@ -23,45 +23,45 @@ var natNewsImage5 = document.getElementById("nationalNewsImage5")
 var natNewsLink5 = document.getElementById("nationalNewsLink5")
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-Api-Key': '-qHy1yp35g0YE9R12kE1fVlqQEQYRNb2ODk2OX1mP_g',
-		// 'X-RapidAPI-Host': 'newsapi.org'
-	}
+  method: 'GET',
+  headers: {
+    'X-Api-Key': '-qHy1yp35g0YE9R12kE1fVlqQEQYRNb2ODk2OX1mP_g',
+    // 'X-RapidAPI-Host': 'newsapi.org'
+  }
 };
-    // fetch for newscatcherapi
+// fetch for newscatcherapi
 function getNationalNews(nationalNewsUrl) {
-	fetch(nationalNewsUrl, options)
-		.then(response => response.json())
-		.then(response => {
-			console.log(response)
-			// appending first article
-			natNewsHeadline.textContent = response.articles[0].title
-			natNewsDescription.textContent = response.articles[0].excerpt
-			natNewsImage.src = response.articles[0].media
-			natNewsLink.textContent = response.articles[0].link
-			// appending 2nd article
-			natNewsHeadline2.textContent = response.articles[1].title
-			natNewsDescription2.textContent = response.articles[1].excerpt
-			natNewsImage2.src = response.articles[1].media
-			natNewsLink2.textContent = response.articles[1].link
-			// appending 3rd article
-			natNewsHeadline3.textContent = response.articles[6].title
-			natNewsDescription3.textContent = response.articles[6].excerpt
-			natNewsImage3.src = response.articles[6].media
-			natNewsLink3.textContent = response.articles[6].link
-			// appending 4th article
-			natNewsHeadline4.textContent = response.articles[7].title
-			natNewsDescription4.textContent = response.articles[7].excerpt
-			natNewsImage4.src = response.articles[7].media
-			natNewsLink4.textContent = response.articles[7].link
-			// appending 5th article
-			natNewsHeadline5.textContent = response.articles[12].title
-			natNewsDescription5.textContent = response.articles[12].excerpt
-			natNewsImage5.src = response.articles[12].media
-			natNewsLink5.textContent = response.articles[12].link
-		})
-		.catch(err => console.error(err))
+  fetch(nationalNewsUrl, options)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      // appending first article
+      natNewsHeadline.textContent = response.articles[0].title
+      natNewsDescription.textContent = response.articles[0].excerpt
+      natNewsImage.src = response.articles[0].media
+      natNewsLink.textContent = response.articles[0].link
+      // appending 2nd article
+      natNewsHeadline2.textContent = response.articles[1].title
+      natNewsDescription2.textContent = response.articles[1].excerpt
+      natNewsImage2.src = response.articles[1].media
+      natNewsLink2.textContent = response.articles[1].link
+      // appending 3rd article
+      natNewsHeadline3.textContent = response.articles[6].title
+      natNewsDescription3.textContent = response.articles[6].excerpt
+      natNewsImage3.src = response.articles[6].media
+      natNewsLink3.textContent = response.articles[6].link
+      // appending 4th article
+      natNewsHeadline4.textContent = response.articles[7].title
+      natNewsDescription4.textContent = response.articles[7].excerpt
+      natNewsImage4.src = response.articles[7].media
+      natNewsLink4.textContent = response.articles[7].link
+      // appending 5th article
+      natNewsHeadline5.textContent = response.articles[12].title
+      natNewsDescription5.textContent = response.articles[12].excerpt
+      natNewsImage5.src = response.articles[12].media
+      natNewsLink5.textContent = response.articles[12].link
+    })
+    .catch(err => console.error(err))
 
 }
 
@@ -70,26 +70,26 @@ getNationalNews(nationalNewsUrl)
 // function to convert state abreviation into state name for display on page
 // api reference link: https://rapidapi.com/saikatjahan50/api/states2/
 function getStateName(cardNum, stateAbrev) {
-	// calls the api that returns an array of state names with key being the abreviation
-	var stateToReturn
-	var options = {
-		method: 'GET',
-		headers: {
-			Country: 'USA',
-			'X-RapidAPI-Key': 'df8820cc33mshf0f6df2756e034fp1ec1bajsna5813e74bc2c',
-			'X-RapidAPI-Host': 'states2.p.rapidapi.com'
-		}
-	};
-	// fetch the data
-	fetch('https://states2.p.rapidapi.com/query?country=USA', options)
-		.then(function (response) {
-			return response.json();
-		}).then(function (data) {
-			// assign a variable to the state name at the relevant location
-			stateToReturn = data[stateAbrev];
-			$(`#stateName${cardNum}`).html(stateToReturn);
-		})
-		.catch(err => console.error(err));
+  // calls the api that returns an array of state names with key being the abreviation
+  var stateToReturn
+  var options = {
+    method: 'GET',
+    headers: {
+      Country: 'USA',
+      'X-RapidAPI-Key': 'df8820cc33mshf0f6df2756e034fp1ec1bajsna5813e74bc2c',
+      'X-RapidAPI-Host': 'states2.p.rapidapi.com'
+    }
+  };
+  // fetch the data
+  fetch('https://states2.p.rapidapi.com/query?country=USA', options)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      // assign a variable to the state name at the relevant location
+      stateToReturn = data[stateAbrev];
+      $(`#stateName${cardNum}`).html(stateToReturn);
+    })
+    .catch(err => console.error(err));
 
 }
 
@@ -99,44 +99,53 @@ var stateNewsURL = 'https://api.legiscan.com/?key=9bf6fb64823ce819fdf2cf635e983c
 
 // function to call legiscan api and handle through JSON
 function getStateNews(stateNewsURL) {
-	fetch(stateNewsURL).then(function (response) {
-		return response.json();
-	}).then(function (data) {
-		console.log(data);
-		// data has now ben handled into an object, now to display to cards
-		// for loop assigns content to cards, looping through 3 times to reduce repetitive hardcoding
-		for (var i = 0; i < 3; i++) {
-			// variables for page display are pulled from api data
-			var BillState = data.searchresult[i].state;
-			var BillNumber = data.searchresult[i].bill_number;
-			var Billtitle = data.searchresult[i].title;
-			var BillURL = data.searchresult[i].url;
-			var BillStatus = data.searchresult[i].last_action;
-			var BillLastAction = data.searchresult[i].last_action_date;
+  fetch(stateNewsURL).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log(data);
+    // data has now ben handled into an object, now to display to cards
+    // for loop assigns content to cards, looping through 3 times to reduce repetitive hardcoding
+    for (var i = 0; i < 3; i++) {
+      // variables for page display are pulled from api data
+      var BillState = data.searchresult[i].state;
+      var BillNumber = data.searchresult[i].bill_number;
+      var Billtitle = data.searchresult[i].title;
+      var BillURL = data.searchresult[i].url;
+      var BillStatus = data.searchresult[i].last_action;
+      var BillLastAction = data.searchresult[i].last_action_date;
 
-			// ensure index of for loop aligns with page display numbers
-			var H = i + 1;
-			// set text of bill title with state and id
-			$(`#stateTitle${H}`).html(`${BillState} ${BillNumber}`);
-			// set text of bill content
-			$(`#stateText${H}`).html(`Per author of bill, handles: ${Billtitle}`);
-			// set link to bill information page
-			$(`#stateLink${H}`).html(`<a href="${BillURL}" target="_blank">Bill Information Page</a>`);
-			// set text of current bill status
-			$(`#stateStatus${H}`).html(`As of ${BillLastAction}, the bill's status is: ${BillStatus}`);
+      // ensure index of for loop aligns with page display numbers
+      var H = i + 1;
+      // set text of bill title with state and id
+      $(`#stateTitle${H}`).html(`${BillState} ${BillNumber}`);
+      // set text of bill content
+      $(`#stateText${H}`).html(`Per author of bill, handles: ${Billtitle}`);
+      // set link to bill information page
+      $(`#stateLink${H}`).html(`<a href="${BillURL}" target="_blank">Bill Information Page</a>`);
+      // set text of current bill status
+      $(`#stateStatus${H}`).html(`As of ${BillLastAction}, the bill's status is: ${BillStatus}`);
 
-			// inserts the name of the state into the title area of the card
-			stateFullName = getStateName(H, BillState);
-		}
-	})
-		.catch(err => console.error(err));
+      // inserts the name of the state into the title area of the card
+      stateFullName = getStateName(H, BillState);
+    }
+  })
+    .catch(err => console.error(err));
 }
-
-
 
 // function call for state news cards
 getStateNews(stateNewsURL);
 
-// 9bf6fb64823ce819fdf2cf635e983ccd API key for LegiScan
 
-// AIzaSyCBtLqLJi2y-KT0N09jB3sgO2YJOw3JR_c APi key for custom google search (hunter)
+
+// local storage of email input
+var emailBtn = document.getElementById('emailBtn')
+
+function emailStore() {
+  var emailFoo = document.getElementById('inputEmail')
+  localStorage.setItem('email', emailFoo.value)
+  var key = localStorage.key(emailFoo.value)
+  var newEmail = localStorage.getItem(key)
+  console.log(newEmail)
+}
+// event listener for email storage
+emailBtn.addEventListener('click', emailStore)
